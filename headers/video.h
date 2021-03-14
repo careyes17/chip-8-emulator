60716707051af4840e3fe60b40467b7f8f541689
careyes17 <careyes4820@eagle.fgcu.edu>
2021-03-14 10:53:31 -0400

    added video output support

--- headers/video.h
+++ headers/video.h
@@ -1,1 +1,89 @@
 #pragma once
+#include "SDL2/SDL.h"
+#include <iostream>
+#include <vector>
+
+struct VideoProps {
+    int screenWidth;
+    int screenHeight;
+    int pixelWidth;
+    int pixelHeight;
+    int rows;
+    int cols;
+};
+
+struct FPSUncapped {
+    int frames;
+    int lastTime;
+    void printFPS() {
+        std::cout << "FPS: " << frames << std::endl;
+    }
+    void init() {
+        frames = 0;
+        lastTime = time(NULL);
+    }
+    void attemptPrint() {
+        frames++;
+        int currentTime = time(NULL);
+        if (currentTime - lastTime >= 1) {
+            lastTime = currentTime;
+            printFPS();
+            frames = 0;
+        }
+    }
+};
+
+struct FPSCapped {
+    double oldTime;
+    double accumulator;
+    double currentTime;
+    double deltaTime;
+    int capValue;
+    void init(int fps) {
+        oldTime = SDL_GetTicks();
+        accumulator = 0.0;
+        currentTime = 0.0;
+        deltaTime = 0.0;
+        capValue = fps;
+    }
+    void updateTime() {
+        currentTime = SDL_GetTicks();
+        deltaTime = currentTime - oldTime;
+        oldTime = currentTime;
+        accumulator += deltaTime;
+    }
+    int getNumberOfUpdates() {
+        int numberOfUpdates = 0;
+        while (accumulator > (1000.0/capValue)) {
+            numberOfUpdates++;
+            accumulator -= (1000.0/capValue);
+            if(accumulator < 0) accumulator = 0;
+        }
+        return numberOfUpdates;
+    }
+};
+
+class Video {
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        VideoProps props;
+        
+        double fps;
+        std::vector<char> pixels;
+        
+        bool running;
+
+    public:
+        Video(int screenWidth, int screenHeight, int pixelWidth, int pixelHeight, double fps, std::vector<char>& pixels);
+        ~Video();
+        void init();
+        void pollInput();
+        void update();
+        void render();
+        void loop();
+
+        void updatePixel(int x, int y, bool on);
+        void clear();
+        void drawPixels();
+};
---
+++ video.cpp
@@ -0,0 +1,158 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <vector>
+#include "headers/video.h"
+
+Video::Video(int screenWidth, int screenHeight, int pixelWidth, int pixelHeight, double fps, std::vector<char>& pixels):
+    window(NULL),
+    renderer(NULL),
+    fps(fps),
+    pixels(pixels),
+    running(true) {
+        props.screenWidth = screenWidth;
+        props.screenHeight = screenHeight;
+        props.pixelWidth = pixelWidth;
+        props.pixelHeight = pixelHeight;
+        props.rows = screenHeight/pixelHeight;
+        props.cols = screenWidth/pixelWidth;
+    }
+
+Video::~Video() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
+void Video::init() {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        // create window
+        window = SDL_CreateWindow(
+            "Game",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            props.screenWidth,
+            props.screenHeight,
+            SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Window could not be created. SDL_Error: %s\n", SDL_GetError());
+            running = false;
+        }
+
+        // create renderer
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if ( renderer == NULL ) {
+            printf("Renderer could not be created. SDL_Error: %s\n", SDL_GetError());
+            running = false;
+        }
+    }
+    return;
+}
+
+void Video::pollInput() {
+    SDL_Event ev;
+    while (SDL_PollEvent(&ev) != 0) {
+        switch(ev.type){
+            case SDL_KEYDOWN:
+                switch( ev.key.keysym.sym ){
+                    case SDLK_ESCAPE:
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case SDL_QUIT:
+                running = false;
+                break;
+            default:
+                break;
+            }
+    }
+    return;
+}
+
+void Video::clear() {
+    // clears screen
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+}
+
+void Video::update() {
+    clear();
+    drawPixels();
+    return;
+}
+
+void Video::render() {
+    // Render the rect to the screen
+    SDL_RenderPresent(renderer);
+    return;
+}
+
+void Video::loop() {
+    if (!running) return;
+
+    // init framerate capping
+    // handles all of the timing related to capping FPS
+    FPSCapped gameFPSCap;
+    gameFPSCap.init(fps);
+    
+    // init uncapped FPS
+    // this serves as a measure of how many iterations
+    // happen in the game loop
+    FPSUncapped gameFPS;
+    gameFPS.init();
+
+    update(); // initial update (prevents garbage from being shown)
+
+    // infinite loop: the game loop
+    while(running) {
+        // update capped FPS time
+        gameFPSCap.updateTime();
+
+        pollInput();
+
+        for (int i = 0; i < gameFPSCap.getNumberOfUpdates(); i++) {
+            update(); // update game logic
+        }
+
+        render(); // render to the screen
+
+        // increment uncapped FPS
+        gameFPS.attemptPrint();
+    }
+    return;
+}
+
+void Video::updatePixel(int x, int y, bool on) {
+    pixels[(x * props.cols) + y] = on;
+    return;
+}
+
+void Video::drawPixels() {
+    for (int i = 0; i < props.rows * props.cols; i++) {
+        
+        int y = i / props.cols;
+        int x = i - props.cols*y;
+
+        // adjusting for pixel size
+        y *= props.pixelWidth;
+        x *= props.pixelHeight;
+
+        SDL_Rect r;
+        r.x = x;
+        r.y = y;
+        r.w = props.pixelWidth;
+        r.h = props.pixelHeight;
+
+        // std::cout << x << " " << y << std::endl;
+
+        int color = pixels[i] ? 255 : 0;
+
+        // Render rect
+        SDL_SetRenderDrawColor(renderer, color, color, color, 255);
+        SDL_RenderFillRect(renderer, &r);
+    }
+}
