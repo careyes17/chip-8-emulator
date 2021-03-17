2539a79f2b414412e9c080917a562ed29edc5011
careyes17 <careyes4820@eagle.fgcu.edu>
2021-03-17 14:17:47 -0400

    500hz clock speed in update loop

--- chip8.cpp
+++ chip8.cpp
@@ -2,9 +2,13 @@
 #include "headers/input.h"
 
 void Chip8::update() {
-    printf("3");
+    cpuFPSCap.updateTime();
+    for (int i = 0; i < cpuFPSCap.getNumberOfUpdates(); i++) {
+        // operations happen at 500 hz
+    }
 
     // debug
+    printf("3");
     if (keyPressed(KEY_A)) printf("a");
     for (int i = 0; i < 2048; i++) (*pixels)[i] = rand() % 2;
     cpu.decrementSoundAndTime();
--- headers/chip8.h
+++ headers/chip8.h
@@ -7,6 +7,7 @@
 #define SCREENHEIGHT 480
 #define PIXELWIDTH 15
 #define PIXELHEIGHT 15
+#define CPUHZ 500
 #define FPS 60
 
 class Chip8 : public Engine {
@@ -14,11 +15,13 @@ class Chip8 : public Engine {
         std::vector<char>* pixels;
         char keysPressed[16];
         Cpu cpu;
+        FPSCapped cpuFPSCap;
     public:
         Chip8(std::vector<char>* pixels):
             Engine(SCREENWIDTH, SCREENHEIGHT, PIXELWIDTH, PIXELHEIGHT, FPS, pixels),
             pixels(NULL),
             cpu(){
+                cpuFPSCap.init(CPUHZ);
                 this->pixels = pixels;
             };
         ~Chip8(){};
